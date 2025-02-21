04ad19f9b0ff5cc974ae95d0b4b8dd5595ef2bc6
Lưu Xuân Hòa (HN-BCD-Developper) <hoalx@runsystem.net>
2025-02-21 11:40:32 +0700

    add certification module

--- addons_custom/g_employee/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import hr_employee
-from . import g_employee_certification
--- addons_custom/g_employee/models/hr_employee.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api, _, tools
-
-
-
-class Employee(models.Model):
-    _inherit = 'hr.employee'
-
-    certification_ids = fields.One2many('g.employee.certification', 'employee_id', string='Chứng chỉ')
